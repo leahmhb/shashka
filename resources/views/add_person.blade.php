a0cabf235fe050b5f10719e1cb8d9991881e1a28
Leah Blake <leahmhb@users.noreply.github.com>
2016-02-29 08:55:54 -0500

    Create add_person.blade.php

---
+++ resources/views/add_person.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.master')
+
+@section('title', 'Add Person')
+
+@section('content')
+<div class="page-header"><h1>Add Person <small>Recording horse origins</small></h1></div>
+<form id="add-horse" class="form-horizontal" method="post">
+
+  <div class="row">
+
+    <div class="col-sm-4">
+      <div class="form-group">
+        <label for="person_name" class="col-sm-3 control-label">Username</label>
+        <div class="col-sm-9">
+          <input name="person_name" class="form-control" id="person_name" placeholder="person">              
+        </div>             
+      </div><!--end username-->       
+    </div><!--end col-->
+
+    <div class="col-sm-4">
+      <div class="form-group">
+        <label for="operson_name" class="col-sm-3 control-label">Stable Name</label>
+        <div class="col-sm-9">       
+          <input name="operson_stable_name" class="form-control" id="operson_stable_name" placeholder="stable name">  
+          <input name="operson_stable_prefix" class="form-control" id="operson_stable_prefix" placeholder="stable prefix">   
+        </div>             
+      </div><!--end stable name--> 
+    </div><!--end col-->
+
+    <div class="col-sm-4">    
+      <div class="form-group">
+        <label for="operson_name" class="col-sm-3 control-label">Stable Prefix</label>
+        <div class="col-sm-9"> 
+          <input name="operson_stable_prefix" class="form-control" id="operson_stable_prefix" placeholder="stable prefix">   
+        </div>             
+      </div><!--end stable prefix--> 
+    </div><!--end col-->
+
+  </div><!--end row-->
+
+
+  <div class="form-group">    
+    <button type="submit" class="pull-right btn-lg btn btn-primary">+ Add Person</button>
+  </div>
+</form>
+@endsection
